0942778dbfaf0b5bb7fd2181dad3bf8e8e4ae544
Saul Pereira <saul_pereira@hotmail.com>
2024-12-04 12:32:33 -0600

    157. Styling the Modal via Tailwind CSS
    157. Styling the Modal via Tailwind CSS

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -3,7 +3,7 @@ import { useRef } from 'react';
 import Input from './Input.jsx';
 import Modal from './Modal.jsx';
 
-export default function NewProject({onAdd}) {
+export default function NewProject({onAdd, onCancel}) {
     const modal = useRef();
 
     const title = useRef();
@@ -15,7 +15,7 @@ export default function NewProject({onAdd}) {
         const enteredDescription = description.current.value;
         const enteredDueDate = dueDate.current.value;
 
-        if (enteredTitle.trim() === '' || enteredDescription() === '' || enteredDueDate.trim() === '') {
+        if (enteredTitle.trim() === '' || enteredDescription.trim() === '' || enteredDueDate.trim() === '') {
             modal.current.open();
             return;
         }
@@ -30,15 +30,15 @@ export default function NewProject({onAdd}) {
     return (
         <>
         <Modal ref={modal} buttonCaption="Okay">
-            <h2>Invalid Input</h2>
-            <p>Oops ... looks like you forgot to enter a value.</p>
-            <p>Please make sure you provide a valid value for every input field.</p>
+            <h2 className='text-xl font-bold text-stone-700 my-4'>Invalid Input</h2>
+            <p className='text-stone-600 mb-4'>Oops ... looks like you forgot to enter a value.</p>
+            <p className='text-stone-600 mb-4'>Please make sure you provide a valid value for every input field.</p>
         </Modal>
         <div className='w-[35rem] mt-16'>
             <menu className='flex items-center justify-end gap-4 my-4'>
                 <li>
                     <button 
-                        className='text-stone-800 hover:text-stone-950'>
+                        className='text-stone-800 hover:text-stone-950' onClick={onCancel}>
                             Cancel
                     </button>
                 </li>
